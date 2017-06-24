1f68b546e8f7ecf6c2b6de92907499371159f585
A---- <A----@users.noreply.github.com>
2017-06-24 17:07:52 +0200

    Refactoring tests too

--- src/test/java/com/xspeedit/robby/PackagerTest.java
+++ src/test/java/com/xspeedit/robby/PackagerTest.java
@@ -13,44 +13,8 @@ import java.util.Collection;
 
 public class PackagerTest
 {
-    @Test
-    public void testPackageBoxValid() {
-        Collection<Integer> input = Arrays.asList(1, 6, 3, 8, 4);
-        Collection<Collection<Integer>> expected = new ArrayList<>();
-        expected.add(Arrays.asList(1, 6, 3));
-        expected.add(Arrays.asList(8));
-        expected.add(Arrays.asList(4));
-
-        Packager packager = new Packager(new BasicPackagingStrategy());
-        Collection<Collection<Integer>> actual = packager.packageBoxes(input);
-
-        assertThat(actual, notNullValue());
-        assertThat(actual, is(expected));
-    }
-
-    @Test
-    public void testPackageBoxExactSize() {
-        Collection<Integer> input = Arrays.asList(1, 6, 3, 8, 2);
-        Collection<Collection<Integer>> expected = new ArrayList<>();
-        expected.add(Arrays.asList(1, 6, 3));
-        expected.add(Arrays.asList(8, 2));
-
-        Packager packager = new Packager(new BasicPackagingStrategy());
-        Collection<Collection<Integer>> actual = packager.packageBoxes(input);
-
-        assertThat(actual, notNullValue());
-        assertThat(actual, is(expected));
-    }
-
-    @Test
-    public void testPackageBoxEmpty() {
-        Collection<Integer> input = Arrays.asList();
-        Collection<Collection<Integer>> expected = new ArrayList<>();
-
-        Packager packager = new Packager(new BasicPackagingStrategy());
-        Collection<Collection<Integer>> actual = packager.packageBoxes(input);
-
-        assertThat(actual, notNullValue());
-        assertThat(actual, is(expected));
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructorInvalid() {
+        new Packager(null);
     }
 }
